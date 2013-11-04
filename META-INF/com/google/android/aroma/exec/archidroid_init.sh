5fcde02fcdd9266991207feb2d5e432c2344f2f9
mrtur <mrturmrtur@gmail.com>
2013-11-04 19:27:06 +0100

    fix /AromaPreset/Nexus4 -> /AromaPreset/slim

--- META-INF/com/google/android/aroma/exec/archidroid_init.sh
+++ META-INF/com/google/android/aroma/exec/archidroid_init.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /data/media/0/ArchiDroid
 mkdir -p /data/media/0/ArchiDroid/AromaPreset
-mkdir -p /data/media/0/ArchiDroid/AromaPreset/Nexus4
+mkdir -p /data/media/0/ArchiDroid/AromaPreset/slim
 
 exit 0
