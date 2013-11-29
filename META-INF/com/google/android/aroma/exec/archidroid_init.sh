4917d214171c8cfa679dff8a0725396d2fe7de97
mrtur <mrturmrtur@gmail.com>
2013-11-29 19:38:13 +0100

    Revert "test"
    This reverts commit b0216debf86335d542c735dc92fed49130c745df.

--- META-INF/com/google/android/aroma/exec/archidroid_init.sh
+++ META-INF/com/google/android/aroma/exec/archidroid_init.sh
@@ -1,27 +1,7 @@
 #!/sbin/sh
 
-# Mounts
-# Used by ArchiDroid for providing universal device-based paths
-fs="ext4" # Filesystem of blocks [USED]
-boot="/dev/block/mmcblk0p6" # ROM's kernel image, not used
-recovery="/dev/block/mmcblk0p7" # Recovery image, not used
-radio="/dev/block/mmcblk0p1" # Modem image, not used
-cache="/dev/block/mmcblk0p22" # Temporary cache, not used
-system="/dev/block/mmcblk0p21" # Main system partition, [USED]
-data="/dev/block/mmcblk0p23" # Internal SD Card, [USED]
-extsd="/dev/block/mmcblk1p1" # External SD Card, not used
-
-AD="/data/media/0/ArchiDroid"
-
-# Prepare all used mounts
-# Firstly try to mount automatically, eventually fallback to predefined blocks
-mount -t auto /data > /dev/null 2>&1
-mount -t $fs $data /data > /dev/null 2>&1
-mount -t auto /system > /dev/null 2>&1
-mount -t $fs $system /system > /dev/null 2>&1
-
-# Init
-mkdir -p $AD
-mkdir -p $AD/AromaPreset/slim
+mkdir -p /data/media/0/ArchiDroid
+mkdir -p /data/media/0/ArchiDroid/AromaPreset
+mkdir -p /data/media/0/ArchiDroid/AromaPreset/slim
 
 exit 0
