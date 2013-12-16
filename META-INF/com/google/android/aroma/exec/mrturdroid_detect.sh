010aa781843bf5789954a0342cf0c416b992b589
mrtur <mrturmrtur@gmail.com>
2013-12-16 16:12:53 +0100

    adpatch, universal mounts, experimental

--- META-INF/com/google/android/aroma/exec/mrturdroid_detect.sh
+++ META-INF/com/google/android/aroma/exec/mrturdroid_detect.sh
@@ -4,21 +4,18 @@
 # exit 1 -> All fine, we're NOT running MrturDroid
 # exit 2 -> All fine, we're running MrturDroid
 
-mount -t auto /system > /dev/null 2>&1
+
 if [ -e /system/build.prop ]; then
 	# Good
 	if [ `cat /system/build.prop | grep -i "MrturDroid" | wc -l` -gt 0 ]; then
 		# Yay we're running MrturDroid"
-		umount /system > /dev/null 2>&1
 		exit 2
 	else
 		# We're not running MrturDroid
-		umount /system > /dev/null 2>&1
 		exit 1
 	fi
 else
 	# Report internal error
-	umount /system > /dev/null 2>&1
 	exit 0
 fi
 exit 0
