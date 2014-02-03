9ff3ada595f0ac16a1b3cd98c39a942b948bb04f
mrtur <mrturmrtur@gmail.com>
2014-02-03 07:14:51 +0100

    Adapt these misc fixes also to adumount.sh

--- META-INF/com/google/android/aroma/exec/adumount.sh
+++ META-INF/com/google/android/aroma/exec/adumount.sh
@@ -5,10 +5,21 @@
 # Used by ArchiDroid for providing universal device-based paths
 # Usage: adumount.sh *path*, f.e. adumount.sh /storage/sdcard1
 
-AUTO="/system /cache /preload /data /storage/sdcard1" # Filesystems which should be unmounted automatically when no argument is given, typically every partition excluding images
+# These are absolute paths without slashes, for example /storage/sdcard1 is storagesdcard1, because you can't use / in variables
+efs="/dev/block/mmcblk0p3" # EFS, if available
+boot="/dev/block/mmcblk0p5" # ROM's kernel image
+recovery="/dev/block/mmcblk0p6" # Recovery image
+radio="/dev/block/mmcblk0p7" # Modem image
+cache="/dev/block/mmcblk0p8" # Cache partition
+system="/dev/block/platform/msm_sdcc.1/by-name" # System partition
+preload="/dev/block/mmcblk0p10" # Preload partition (also SELinux)
+data="/dev/block/mmcblk0p12" # Data and internal memory
+storagesdcard1="/dev/block/mmcblk1p1" # External memory, if available
+AUTO="/efs /system /cache /preload /data /storage/sdcard1" # Filesystems which should be unmounted automatically when no argument is given, typically every partition excluding images
 
 GOTBUSYBOX=false
 GOTMOUNT=false
+LOG="/tmp/mrturdroid_mount.log" # We can use /dev/null if not required
 
 ADMOUNTED() {
 	if [ `mount | grep -i "$1" | wc -l` -gt 0 ]; then
@@ -20,52 +31,51 @@ ADMOUNTED() {
 
 ADUMOUNT() {
 	if (ADMOUNTED "$1"); then
-		# Automatic filesystem
+		MNTPATH=`echo $1 | sed 's/\///g'`
+		eval "MNTPATH=\$$MNTPATH"
 		if $GOTBUSYBOX; then
-			busybox umount "$1" >/dev/null 2>&1
+			busybox umount -f "$1" >/dev/null 2>&1
+			busybox umount -f "$MNTPATH" >/dev/null 2>&1 # This is required for freeing up block path completely, used for example in reformatting
 			if !(ADMOUNTED "$1"); then
-				echo "Unmounted $1 through busybox umount"
+				echo "Successfully unmounted $1 through busybox umount" >> $LOG
 				return 0
 			fi
 		fi
 		if $GOTMOUNT; then
-			umount "$1" >/dev/null 2>&1
+			umount -f "$1" >/dev/null 2>&1
+			umount -f "$MNTPATH" >/dev/null 2>&1 # This is required for freeing up block path completely, used for example in reformatting
 			if !(ADMOUNTED "$1"); then
-				echo "Unmounted $1 through umount"
+				echo "Successfully unmounted $1 through umount" >> $LOG
 				return 0
 			fi
 		fi
 		# Ok, I give up
 		if (ADMOUNTED "$1"); then
-			echo "Could not unmount $1"
+			echo "ERROR: Could not unmount $1" >> $LOG
 			return 1
 		fi
 	else
-		echo "$1 is already unmounted"
+		echo "$1 is already unmounted" >> $LOG
 	fi
 	return 0
 }
 
 if [ ! -z `which busybox` ]; then
-	echo "Got busybox!"
 	GOTBUSYBOX=true
 fi
 if [ ! -z `which mount` ]; then
-	echo "Got mount!"
 	GOTMOUNT=true
 fi
 if (! $GOTBUSYBOX && ! $GOTMOUNT); then
 	# This should never happen, but safety check is always good
-	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT"
+	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT" >> $LOG
 	exit 1
 fi
 
 if [ -z "$1" ]; then
 	# No argument given, umount AUTO
 	for mnt in $AUTO; do
-		if [ ! -z "$mnt" ]; then
-			ADUMOUNT "$mnt"
-		fi
+		ADUMOUNT "$mnt"
 	done
 else
 	ADUMOUNT "$1"
