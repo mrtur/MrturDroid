d00de55b3c6636760e6d27caa8183ee70cc16861
mrtur <mrturmrtur@gmail.com>
2015-05-19 09:01:21 +0200

    test

--- _mrturdroid/kernels/faux/system/etc/init.d/99zram.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-
-# activate zram swap!
-if [ -f /sys/devices/virtual/block/zram0/disksize ]; then
-        echo 419430400 > /sys/devices/virtual/block/zram0/disksize
-        mkswap /dev/block/zram0
-        swapon /dev/block/zram0
-fi
-
